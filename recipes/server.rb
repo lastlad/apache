5f397772007770234e9e95d6ce904ac830120cf7
Santosh Vemu <vemusantosh@gmail.com>
2019-02-24 21:56:35 +0000

    Added Notifies and Subscribes functionality

--- recipes/server.rb
+++ recipes/server.rb
@@ -6,8 +6,10 @@ template '/var/www/html/index.html' do
   source 'index.html.erb'
   variables(:name => 'Santosh')
   action :create
+  notifies :restart, 'service[httpd]', :immediately
 end
 
 service 'httpd' do
   action [:enable, :start]
+#  subscribes :restart, 'template[/var/www/html/index.html]', :immediately
 end
